ccac8f39c9cd7a7fc73dd5f6f0208b96e26d2dbf
Jayasri1702 <110477471+Jayasri1702@users.noreply.github.com>
2023-12-26 17:04:30 +0530

    Update page.tsx

--- app/reservations/page.tsx
+++ app/reservations/page.tsx
@@ -7,6 +7,8 @@ import getReservations from "@/app/actions/getReservations";
 
 import TripsClient from "./ReservationsClient";
 
+export const dynamic = 'force-dynamic'
+
 const ReservationsPage = async () => {
   const currentUser = await getCurrentUser();
 
